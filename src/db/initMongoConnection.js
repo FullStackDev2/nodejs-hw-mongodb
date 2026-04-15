e318df468b199dbdbfb1f077bef59722df3c8722
Nurettin Dinçer <imrocker32@gmail.com>
2026-04-15 03:36:39 +0300

    all done!

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export const initMongoConnection = async () => {
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
+    process.env;
+
+  const mongoUrl = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+  await mongoose.connect(mongoUrl);
+
+  console.log('Mongo connection successfully established!');
+};
